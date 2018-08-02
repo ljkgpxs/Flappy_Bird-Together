8c281a58909f1bcbfb0c9da9c82c9ad25a8bdaae
ljkgpxs <ljkgpxs@gmail.com>
2018-08-02 10:57:39 +0800

    add audio system
    Signed-off-by: ljkgpxs <ljkgpxs@gmail.com>

---
+++ src/tests/TestSounds.java
@@ -0,0 +1,23 @@
+package tests;
+
+import static java.lang.Thread.sleep;
+
+import utils.AudioPlay;
+
+public class TestSounds {
+    public static void main(String[] args) {
+        AudioPlay.playMusic("resources/sounds/bgm1.wav", 5);
+        try {
+            sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        AudioPlay.stopAll();
+
+        try {
+            sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
